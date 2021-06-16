e34584432d98b8b1e9eeaccd4603b89e3f338cac
Anett <holcza@gmail.com>
2021-06-16 17:59:40 +0200

    JUnit tasks 4

--- senior-solutions/src/main/java/locations/LocationService.java
+++ senior-solutions/src/main/java/locations/LocationService.java
@@ -1,5 +1,6 @@
 package locations;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,10 +9,32 @@ import java.util.List;
 
 public class LocationService {
 
-    public List<Location> readLocationsFromCsv(Path path) throws IOException {
+    public void writeLocations(Path file, List<Location> locations) {
+
+        try (FileWriter fileWriter = new FileWriter(String.valueOf(file))) {
+            for (Location l : locations) {
+                fileWriter.write(l.getName() + "," + l.getLat() + "," + l.getLon() + "/n");
+            }
+
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not write file", ioe);
+        }
+
+    }
+
+    public List<Location> readLocationsFromCsv(Path path) {
         List<Location> locations = new ArrayList<>();
 
-        Files.readString(path).split(",");
+        try {
+            List<String> lines = Files.readAllLines(path);
+
+            for (String l : lines) {
+                String[] parts = l.split(",");
+                locations.add(new Location(parts[0], Double.parseDouble(parts[1]), Double.parseDouble(parts[2])));
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
         return locations;
     }
 }
