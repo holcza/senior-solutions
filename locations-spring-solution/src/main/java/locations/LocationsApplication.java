b1415290a12f55264e3510e33ef759d9da0cebe2
Anett <holcza@gmail.com>
2021-06-22 22:43:55 +0200

    Updated status codes and created ExceptionHandler

--- locations-spring-solution/src/main/java/locations/LocationsApplication.java
+++ locations-spring-solution/src/main/java/locations/LocationsApplication.java
@@ -1,5 +1,6 @@
 package locations;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,6 +17,11 @@ public class LocationsApplication {
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
+	}
+
+	@Bean
+	public ObjectMapper objectMapper(){
+		return new ObjectMapper().findAndRegisterModules();
 	}
 
 
--- locations-spring-solution/src/main/java/locations/LocationsController.java
+++ locations-spring-solution/src/main/java/locations/LocationsController.java
@@ -1,7 +1,13 @@
 package locations;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,12 +26,23 @@ public class LocationsController {
         return locationsService.getLocations(name);
     }
 
+    //@GetMapping("/{id}")
+    //public ResponseEntity findLocationById (@PathVariable("id") long id){
+    //    try{
+    //        return ResponseEntity.ok(locationsService.findLocationById(id));
+    //    } catch (IllegalArgumentException iae){
+    //        return ResponseEntity.notFound().build();
+    //    }
+    //}
+
     @GetMapping("/{id}")
     public LocationDto findLocationById (@PathVariable("id") long id){
         return locationsService.findLocationById(id);
+
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public LocationDto createLocation (@RequestBody CreateLocationCommand command){
         return locationsService.createLocation(command);
     }
@@ -36,7 +53,25 @@ public class LocationsController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteLocation (@PathVariable("id") long id){
         locationsService.deleteLocation(id);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Problem> locationNotFoundException(IllegalArgumentException iae){
+        Problem problem = Problem.builder()
+                .withType(URI.create("/locations/not_found"))
+                .withTitle("Not found")
+                .withStatus(Status.NOT_FOUND)
+                .withDetail(iae.getMessage())
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_PROBLEM_JSON)
+                .body(problem);
+
     }
 }
