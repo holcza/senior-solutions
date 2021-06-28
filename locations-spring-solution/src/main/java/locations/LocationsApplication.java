ee725984d7f15970fcf74ca93ccc4e3a97caf728
Anett <holcza@gmail.com>
2021-06-28 10:17:25 +0200

    Comment added to LocationsApplication

--- locations-spring-solution/src/main/java/locations/LocationsApplication.java
+++ locations-spring-solution/src/main/java/locations/LocationsApplication.java
@@ -4,15 +4,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class LocationsApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(LocationsApplication.class, args);
+		ApplicationContext atxt = SpringApplication.run(LocationsApplication.class, args);
+
+		//LocationsController locationsController = atxt.getBean(LocationsController.class);
+
+		//System.out.println(locationsController.getLocations(Optional.empty()));
 	}
+
+
 
 	@Bean
 	public ModelMapper modelMapper(){
