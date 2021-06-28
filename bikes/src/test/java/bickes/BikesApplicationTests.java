b57868a8f5a997728249690148f0c3209f393127
Anett <holcza@gmail.com>
2021-06-28 07:37:33 +0200

    Init bikes project

---
+++ bikes/src/test/java/bickes/BikesApplicationTests.java
@@ -0,0 +1,13 @@
+package bickes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
