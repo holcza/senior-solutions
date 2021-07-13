e6604a0f9acb81713a0bae61a9354e3537b1b246
Anett <holcza@gmail.com>
2021-07-13 21:22:17 +0200

    Saving to db with JPA in main

---
+++ activitytracker/src/main/java/activitytracker/Activity.java
@@ -0,0 +1,63 @@
+package activitytracker;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "activities")
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "start_time")
+    private LocalDateTime startTime;
+
+    @Column(name = "description")
+    private String desc;
+
+    @Enumerated(value = EnumType.STRING)
+    private Type type;
+
+    public Activity() {
+    }
+
+    public Activity(LocalDateTime startTime, String desc, Type type) {
+        this.startTime = startTime;
+        this.desc = desc;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+}
